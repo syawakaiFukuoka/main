ca190aaf7cf41ea1d0f2fd8dec55ea310dd790bf
maphotalk <m.a.photalk@gmail.com>
2020-08-09 22:57:55 +0900

    [Add] V3.3.6(event add)

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ window.onload=function(){
     el: '#app',
     //スライダー
     data: {
-      eventTitle: 'Vol.9 「Outdoor」+ On:Vol.7',
-      eventVolOn: 'Vol.9',
+      eventTitle: 'Online:Vol.7',
+      // eventVolOn: 'Vol.9',
       eventVolOff: 'Vol.7',
-      eventDateOn: '8/8(土) 18:00～',
+      // eventDateOn: '8/8(土) 18:00～',
       eventDateOff: '2020.7.12～8/31',
       current_slide: 0,　//追加
       slides: [
